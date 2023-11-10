03fdf705824c74b7f8a38aaf3dcabef6028addf4
Paul Arntz <paul@paularntz.com>
2023-11-10 17:23:11 -0600

    fix map issue

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -27,7 +27,7 @@ export default async function TopicsList() {
 
     return (
         <>
-        {topics.map( (t) => (
+        {topics?.map( (t) => (
             <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
                     <h2 className="font-bold text-2xl">{t.title}</h2>
