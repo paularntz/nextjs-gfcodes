6693ea8edbb200960f49c69b5aaac5a8c1994726
Paul Arntz <paul@paularntz.com>
2023-11-10 18:36:35 -0600

    add fields 1

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,29 +1,30 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation"
-import { LocaleRouteNormalizer } from "next/dist/server/future/normalizers/locale-route-normalizer";
 
 export default function AddTopic() {
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [violation, setViolation] = useState("");
     const router = useRouter();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         if (!title || !description) {
             alert("Title and description are required.")
             return
         }
-
         try {
             const res = await fetch('https://admirable-conkies-c2b178.netlify.app/api/topics', {
                 method: "POST",
                 headers: {
-                    "Content-type": "application/json" 
-            },
-            body: JSON.stringify({title, description}),
+                    "Content-type": "application/json",
+                    "RequestMode": "no-cors"
+                },
+                body: JSON.stringify({ title, description, firstName, lastName, violation }),
 
             })
             if (res.ok) {
@@ -39,18 +40,46 @@ export default function AddTopic() {
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-            <input 
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                type="text" 
-                placeholder="Topic Title" 
-                className="border border-slate-500 px-8 py-2"
-            />
-            <input 
+            <div className="grid grid-cols-2 gap-4">
+                <input
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                    type="text"
+                    placeholder="First Name"
+                    className="border border-slate-500 px-8 py-2"
+                />
+
+                <input
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
+                    type="text"
+                    placeholder="Last Name"
+                    className="border border-slate-500 px-8 py-2"
+                />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    type="text"
+                    placeholder="Title"
+                    className="border border-slate-500 px-8 py-2"
+                />
+                <select
+                    onChange={(e) => setViolation(e.target.value)}
+                    defaultValue="0"
+                >
+                    <option value="0" disabled>Select One</option>
+                    <option value="Tall Grass">Tall Grass</option>
+                    <option value="Trash In Yard">Trash In Yard</option>
+                    <option value="Downed Tree">Downed Tree</option>
+                    <option value="Illegal Burning">Illegal Burning</option>
+                </select>
+            </div>
+            <textarea
                 onChange={(e) => setDescription(e.target.value)}
                 value={description}
-                type="text" 
-                placeholder="Topic Description" 
+                placeholder="Description"
                 className="border border-slate-500 px-8 py-2"
             />
             <button type="submit" className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
     const {title, description} = await request.json()
     await connectMongoDB()
-    await Topic.create({title, description})
+    await Topic.create({title, description, fistName, lastName, violation})
     return NextResponse.json({message: "Topic Created"}, {status: 201})
 }
 
