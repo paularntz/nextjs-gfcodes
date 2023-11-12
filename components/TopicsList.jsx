cbcff0ee332796e871bfcb410ac356a637206f86
Paul Arntz <paul@paularntz.com>
2023-11-12 12:21:03 -0600

    change listing to include first name

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -30,7 +30,7 @@ export default async function TopicsList() {
         {topics?.map( (t) => (
             <div key={t._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
                 <div>
-                    <h2 className="font-bold text-2xl">{t.title}</h2>
+                    <h2 className="font-bold text-2xl">{t.firstName}</h2>
                     <div>{t.description}</div>
                 </div>
                 <div className="flex gap-2">
