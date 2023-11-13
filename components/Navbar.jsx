020355a05f5cff116fc28f7613944edc61d7a556
Paul Arntz <paul@paularntz.com>
2023-11-13 00:04:44 -0600

    add search button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,8 @@ export default function Navbar() {
     return (
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
             <Link className="text-white font-bold" href={'/'}>GFD Codes Enforcement</Link>
-            <Link className="bg-white p-2" href={'/addTopic'}>Add Topic</Link>
+            <Link className="bg-white p-2" href={'/addTopic'}>Add Contact</Link>
+            <Link className="bg-white p-2" href={'/findAddress'}>Search</Link>
         </nav>
     )
 }
