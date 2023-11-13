6b28056c76217cba1276f9732b02281f70f0c73a
Paul Arntz <paul@paularntz.com>
2023-11-13 00:22:30 -0600

    css tweaks 4

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="flex justify-between items-center bg-slate-800 px-8 py-3">
             <Link className="text-white font-bold" href={'/'}>GFD Codes Enforcement</Link>
             <div>
-                <Link className="bg-white p-2 mx-5" href={'/addTopic'}>Contact</Link>
+                <Link className="bg-white p-2 mx-5" href={'/addTopic'}>Add</Link>
                 <Link className="bg-white p-2" href={'/findAddress'}>Search</Link>
             </div>
         </nav>
